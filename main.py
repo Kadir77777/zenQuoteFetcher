c242368fe93a576d60fce4b5fc67ec505a27d87d
Kadir77777 <lilkadirali12@gmail.com>
2025-12-17 12:53:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import requests
 import json
 import sys
 import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Read webhook URL
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+if not WEBHOOK_URL:
+    print("DISCORD_WEBHOOK_URL not found in .env file")
+    sys.exit(1)
 
 
 response = requests.get("https://zenquotes.io/api/random")
@@ -13,3 +23,12 @@ quote = data[0]['q']
 author = data[0]['a']
 quote_text = f'"{quote}" - {author}'
 print(quote_text)
+payload = {
+"content": quote_text
+}
+
+requests.post(
+WEBHOOK_URL,
+data=json.dumps(payload),
+headers={"Content-Type": "application/json"}
+)
